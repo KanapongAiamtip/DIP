e73608905e26aff2b1ec6dcef5d5460684a8ce43
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-12-11 21:30:12 +0700

    Basic 2

---
+++ Lab Basic Python/P2Q2.py
@@ -0,0 +1,11 @@
+jimmyyjp = int(input())
+if jimmyyjp <37:
+    print("XS")
+elif jimmyyjp >=37 and jimmyyjp <41:
+    print("S")
+elif jimmyyjp >=41 and jimmyyjp <43:
+    print("M")
+elif jimmyyjp >=43 and jimmyyjp <46:
+    print("L")
+else:
+    print("XL")
